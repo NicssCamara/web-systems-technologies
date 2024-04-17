dd8a26dbff6ac5726a031d3125998f27f5f4fe56
Nics-IT2R2 <160290934+Nics-IT2R2@users.noreply.github.com>
2024-04-17 19:52:26 +0800

    draft

--- account-table.js
+++ account-table.js
@@ -1,26 +1,24 @@
-let id = 0;
-
+let ID = 0;
 document.addEventListener("DOMContentLoaded", function() {
-    const regform = document.getElementById("regform");
+    const form = document.getElementById("regisForm");
 
-    regform.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function(event) {
         event.preventDefault();
-
-        append_values(event);
+        appendValues(event);
     });
-    
-    //Getting values
-    function append_values(event) {
+
+    //Getting Values
+    function appendValues(event) {
         const username = document.getElementById("username").value;
         const email = document.getElementById("email").value;
         const role = document.getElementById("role").value;
 
-        const row = document.getElementById("tr").value;
+        const row = document.createElement('tr');
 
-        row.innerHTML = ` 
-        <td style="px-6 py-4 whitespace-nowrap">${++id}</td>
-        <td style="px-6 py-4 whitespace-nowrap">${username}</td>
-        <td style="px-6 py-4 whitespace-nowrap">${email}</td>
+        row.innerHTML = `
+        <td style="border-right: 2px solid #4b5563; text-align: center;">${++ID}</td>
+        <td style="border-right: 2px solid #4b5563; text-align: center;">${username}</td>
+        <td style="border-right: 2px solid #4b5563; text-align: center;">${email}</td>
         <td style="border-right: 2px solid #4b5563; text-align: center;">${role}</td>
         <td style="text-align: center;">
             <button class="edit" style="margin-right: 10px;">
@@ -29,23 +27,23 @@ document.addEventListener("DOMContentLoaded", function() {
             <button class="delete">
                 <i class="fas fa-trash" style="color: red;"></i>
             </button>
-        </td>
-    `;
+        </td>`;
 
-    //Append Row
-    const tbody = document.querySelector(".table-auto tbody");
+    //Adding Rows
+    const tbody = document.querySelector('.table-auto tbody');
     tbody.appendChild(row);
 
-    row.querySelector(".edit").addEventListener("click", editRow);
-    row.querySelector(".delete").addEventListener("click", deleteRow);
+    row.querySelector('.edit').addEventListener('click', editRow);
+    row.querySelector('.delete').addEventListener('click', deleteRow);
 
     row.style.border = '2px solid #4b5563';
     row.style.borderRight = '2px solid #4b5563';
 
     }
 
-    //Edit row
-    function editRow(event){
+
+    //Edit Row
+    function editRow(event) {
         const button = event.target.parentNode;
         const row = button.parentNode.parentNode;
         const username = row.children[1];
@@ -59,6 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
         button.innerHTML = '<i class="fas fa-save" style="color: green;"></i>';
         button.removeEventListener('click', editRow);
         button.addEventListener('click', saveRow);
+
     }
 
     //Save Row
@@ -68,21 +67,20 @@ document.addEventListener("DOMContentLoaded", function() {
         const username = row.children[1].querySelector('input');
         const email = row.children[2].querySelector('input');
         const role = row.children[3].querySelector('input');
-
+    
         username.parentNode.textContent = username.value;
         email.parentNode.textContent = email.value;
         role.parentNode.textContent = role.value;
-
+    
         button.innerHTML = '<i class="fas fa-edit" style="color: blue;"></i>';
         button.removeEventListener('click', saveRow);
         button.addEventListener('click', editRow);
     }
 
-    // Delete row 
+    //Delete Row
     function deleteRow(event) {
         const button = event.target.parentNode;
         const row = button.parentNode.parentNode;
         row.parentNode.removeChild(row);
     }
-})
-
+});
